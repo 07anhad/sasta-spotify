20f1a0749678d3b4b7348a9643a31bcfdde21e22
Anhad <84476084+07anhad@users.noreply.github.com>
2022-11-27 13:12:29 +0530

    spotifyXtiktok

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,10 @@
 import loader from './loader.svg';
 import logoo from './logoo.png';
+import logo3 from './logo3.jpeg';
+
 
 export {
   logoo,
   loader,
+  logo3
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { HiOutlineHashtag, HiOutlineHome, HiOutlineMenu, HiOutlinePhotograph, HiOutlineUserGroup } from 'react-icons/hi';
 import { RiCloseLine } from 'react-icons/ri';
 
-import { logoo } from '../assets';
+import { logo3 } from '../assets';
 
 const links = [
   { name: 'Discover', to: '/', icon: HiOutlineHome },
@@ -34,7 +34,9 @@ const Sidebar = () => {
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-        <img src={logoo} alt="logo" className="w-full h-14 object-contain" />
+        <img src={logo3} alt="logo" className="w-full h-14 object-contain" />
+        <br></br>
+        <h2 className="text-slate-300 font-bold mx-9">Spotify X Tiktok</h2>
         <NavLinks />
       </div>
 
@@ -48,7 +50,8 @@ const Sidebar = () => {
       </div>
 
       <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
-        <img src={logoo} alt="logo" className="w-full h-14 object-contain" />
+        <img src={logo3} alt="logo" className="w-full h-14 object-contain" />
+        <br></br>
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
